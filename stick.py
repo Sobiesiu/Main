b2d5f89bdbb3f258afa7b2c747642e083ebeeeaa
Tomasz Sobiesiak <sobiesiu@gmail.com>
2020-06-01 21:18:03 +0200

    hmmm

--- stick.py
+++ stick.py
@@ -1,2 +1,23 @@
+import requests
+from requests_html import HTMLSession
 
+url = 'https://www.similarweb.com/website/superpharm.pl'
 
+try:
+    session = HTMLSession()
+    r = session.get(url)
+except requests.exceptions.RequestException as e:
+    print(e)
+
+get_engagement_values = r.html.find('.engagementInfo-valueNumber')
+
+engagement_names = {'Total Visits': '', 'Bounce Rate': '', 'Avg. Visit Duration': '', 'Pages per Visit': ''}
+
+engagement_values = []
+
+for i in range(len(get_engagement_values)):
+    engagement_values.append(get_engagement_values[i].text)
+
+engagement = dict(zip(engagement_names, engagement_values))
+
+print(engagement)
